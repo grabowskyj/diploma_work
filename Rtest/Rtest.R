d80f2b6771642a6c50b2b94dbd2ebdcb828ac75b
Peter Szabo <89peterszabo@gmail.com>
2017-02-14 23:31:47 +0100

    [test] adding R test script

---
+++ Rtest/Rtest.R
@@ -0,0 +1,25 @@
+# TODO: Add comment
+# 
+# Author: Péter
+###############################################################################
+
+# Print text
+print("Hello R")
+
+# Print sum
+print(2 + 5)
+
+# Creating vector
+v = c(1,2,4,5)
+
+# Calculate mean value of v
+vm = mean(v)
+
+# Calculate median of v
+mv = median(v,FALSE)
+
+# Print value of vm
+print(vm)
+
+# Print value of mv
+print(mv)
