d30e612bb44c693c27e6209cfa5a0ac934416238
Peter Szabo <89peterszabo@gmail.com>
2017-03-27 02:13:32 +0200

    converter update: creating raw data working

--- predLocMobHand/src/com/taylor/simulation/ConvertDatFile.java
+++ predLocMobHand/src/com/taylor/simulation/ConvertDatFile.java
@@ -1,17 +1,26 @@
 package com.taylor.simulation;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import sun.text.normalizer.RangeValueIterator;
 
 public class ConvertDatFile {
     
     private File datFile;
+    private File convFile;
+    private File csvFile;
 
-    ConvertDatFile(File datFile) {
+    ConvertDatFile(File datFile, File convFile, File csvFile) {
         this.setDatFile(datFile);
+        this.setConvFile(convFile);
+        this.setCsvFile(csvFile);
     }
 
     public File getDatFile() {
@@ -22,83 +31,158 @@ public class ConvertDatFile {
         this.datFile = datFile;
     }
     
-    public ArrayList<ArrayList<String>> readDatFile() {
+    public File getconvFile() {
+        return convFile;
+    }
+
+    public void setConvFile(File convFile) {
+        this.convFile = convFile;
+    }
+    
+    public File getCsvFile() {
+        return csvFile;
+    }
+
+    public void setCsvFile(File csvFile) {
+        this.csvFile = csvFile;
+    }
+    
+    public void createRawData() {
         FileInputStream readFile = null;
+        FileWriter fileWriter = null;
+        BufferedWriter bufferedWriter = null;
         ArrayList<String> dataRow = new ArrayList<String>();
-        ArrayList<ArrayList<String>> headerData = new ArrayList<ArrayList<String>>();
-        ArrayList<ArrayList<String>> bestServerData = new ArrayList<ArrayList<String>>();
-        ArrayList<ArrayList<String>> fileData = new ArrayList<ArrayList<String>>();
+        int carriageReturn = 0;
+        int read;
         String hexNewLine = "a";
         String hexCarriageReturn = "d";
         String readedChar = null;
-        int read;
-        /*int eastMin = 0;
-        int eastMax = 0;
-        int northMin = 0;
-        int northMax = 0;
-        int resolution = 0;
-        
-        ezek majd feldolgozasnal kellenek*/
         
         try {
             readFile = new FileInputStream(getDatFile());
-            int c = 0;
+            fileWriter = new FileWriter(getconvFile());
+            bufferedWriter = new BufferedWriter(fileWriter);
             while((read = readFile.read()) != -1) {
                 readedChar = Integer.toHexString(read);
                 if (!readedChar.equals(hexCarriageReturn) && !readedChar.equals(hexNewLine)) {
                     dataRow.add(readedChar);
-                    if ((headerData.size() == 2) && (dataRow.size() == 10)) {
-                        bestServerData.add(dataRow);
+                    if ((carriageReturn == 3) && (dataRow.size() == 10)) {
+                        for (String data : dataRow) {
+                            bufferedWriter.write(data + " ");
+                        }
+                        bufferedWriter.newLine();
                         dataRow.clear();
                     }
                 }
-                if (readedChar.equals(hexCarriageReturn) && (dataRow.size() > 0)) {
-                    headerData.add(dataRow);
-                    for (ArrayList<String> i : headerData) {
-                        System.out.print(i + " ");
+                if (readedChar.equals(hexCarriageReturn)) {
+                    if (dataRow.size() > 0) {
+                        for (String data : dataRow) {
+                            int data2decimal = Integer.parseInt(data, 16);
+                            char character = (char) data2decimal; 
+                            bufferedWriter.write(character);
+                        }
+                        bufferedWriter.newLine();
+                    } else {
+                    	bufferedWriter.write("NO ENTRY");
+                    	bufferedWriter.newLine();
                     }
+                    carriageReturn++;
                     dataRow.clear();
                 }
+            }
+        } catch(Exception e) {
+            System.out.println(e);
+            e.printStackTrace();
+        } finally {
+            try {
+                readFile.close();
+                bufferedWriter.close();
+            } catch (Exception e) {
+                System.out.println(e);
+                e.printStackTrace();
+            }  
+        }
+    }
+    
+    public void convertRawData() {
+        FileReader fileReader = null;
+        FileWriter fileWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        ArrayList<String> rawData = new ArrayList<String>();
+        int eastMin = 0;
+        int eastMax = 0;
+        int northMin = 0;
+        int northMax = 0;
+        int resolution = 0;
+        int cellID;
+        int layerID;
+        int signalStrength;        
                 
-                //System.out.print(Integer.toHexString(read) + " ");
-                c++;
-                if (c == 150) {
-                    //System.exit(1);
-                    break;
-                }
+        try {
+            fileReader = new FileReader(getconvFile());
+            bufferedReader = new BufferedReader(fileReader);
+            for (String readedLine; (readedLine = bufferedReader.readLine()) != null;) {
+                rawData.add(readedLine.trim());
             }
-            readFile.close();
-            fileData.addAll(headerData);
-            System.out.println("File Data Size: " + fileData.size());
-            fileData.addAll(bestServerData);
-            System.out.println("File Data Size: " + fileData.size());
-            System.out.println("Header Data Size: " + headerData.size());
-            System.out.println("Best Server Data Size: " + bestServerData.size());
-            System.out.print("Header Data: ");
-            for (ArrayList<String> i : headerData) {
-                System.out.print(i + " ");
-            }
-            System.out.println("\n");
-            System.out.print("Best Server Data: ");
-            for (ArrayList<String> i : bestServerData) {
-                System.out.print(i + " ");
+            String[] mapInfo = rawData.get(1).split(" ");
+            eastMin = Integer.parseInt(mapInfo[0]);
+            eastMax = Integer.parseInt(mapInfo[1]);
+            northMin = Integer.parseInt(mapInfo[2]);
+            northMax = Integer.parseInt(mapInfo[3]);
+            resolution = Integer.parseInt(mapInfo[4]);
+            
+            for (int rowNum = 3; rowNum <= rawData.size(); rowNum++) {
+            	String[] processedLine = rawData.get(rowNum).split(" ");
+            	String[] hexCellID = new String[]{processedLine[3],processedLine[2],processedLine[1],processedLine[0]};
+            	String[] hexLayerID = new String[]{processedLine[7],processedLine[6],processedLine[5],processedLine[4]};
+            	String[] hexSignalStrength = new String[]{processedLine[9],processedLine[8]};
+            	//csinalni egy fuggvenyt, ami csak a jo sorrendben levo hex arrayt forditja at decimalisra
+            	//csinalni egy fuggvenyt, ami osszerakja a csv-be beirando sort a jo koordinatakkal, amit persze elotte ki kell szamolni valahogy
+            	
             }
             
         } catch(Exception e) {
             System.out.println(e);
             e.printStackTrace();
+        } finally {
+            try {
+                fileReader.close();
+                bufferedReader.close();
+            } catch (Exception e) {
+                System.out.println(e);
+                e.printStackTrace();
+            }  
         }
-        
-        return fileData;
     }
     
     public static void main(String[] args) {
         
-        File simFile = new File("C:\\Users\\eptrszb\\git\\diploma_work\\minta1_25m_bestserver.dat");
-        ArrayList<ArrayList<String>> simDataInList = new ArrayList<ArrayList<String>>();
-        ConvertDatFile simData = new ConvertDatFile(simFile);
+        File simFile = new File("D:\\Downloads\\OneDrive\\Documents\\Suli\\MSc\\Diplomamunka\\Mérések\\Aircom array\\Minta\\minta1_25m_bestserver.dat");
+        File convFile = new File("D:\\Dokumentumok\\GIT\\diploma_work\\minta1_25m_bestserver.conv");
+        File csvFile = new File("D:\\Dokumentumok\\GIT\\diploma_work\\minta1_25m_bestserver.csv");
         
-        simDataInList = simData.readDatFile();
+        if (convFile.exists()) {
+            convFile.delete();
+        }
+        
+        if (csvFile.exists()) {
+            csvFile.delete();
+        }
+        
+        try {
+            convFile.createNewFile();
+            csvFile.createNewFile();
+        } catch (IOException e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+        
+        ConvertDatFile baseData = new ConvertDatFile(simFile, convFile, csvFile);
+        
+        baseData.createRawData();
+        baseData.convertRawData();
+        
     }
     
 }
