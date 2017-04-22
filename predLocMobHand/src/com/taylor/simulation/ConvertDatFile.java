98b908cf6e874a546f4938cb4f90e6af3b453762
Peter Szabo <89peterszabo@gmail.com>
2017-04-22 13:46:16 +0200

    refactoring convertDatFile

--- predLocMobHand/src/com/taylor/simulation/ConvertDatFile.java
+++ predLocMobHand/src/com/taylor/simulation/ConvertDatFile.java
@@ -7,6 +7,8 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Hashtable;
+import java.util.List;
+
 import com.taylor.tools.*;
 import com.taylor.tools.Tools.FILETYPE;
 
@@ -76,7 +78,7 @@ public class ConvertDatFile {
         BufferedWriter bufferedWriter = null;
         StringBuilder numOfServers = null;
         StringBuilder headerRow = null;
-        ArrayList<String> dataRow = new ArrayList<String>();
+        ArrayList<String> dataRow = null;
         boolean isNumOfServersSet = false;
         int carriageReturnSensor = 0;
         int emptyLineSize = 2;
@@ -84,35 +86,46 @@ public class ConvertDatFile {
         int numOfServersInt = 0;
         int numOfServersCharLength = 2;
         int serverCounter = 0;
+        int read;
+        char character = 0;
         String hexNewLine = "0a";
         String hexCarriageReturn = "0d";
         String zeroChar = "0";
         String readedChar = null;
         
+        dataRow = new ArrayList<String>();
+        
         try {
             readFile = new FileInputStream(getSrcFile());
             fileWriter = new FileWriter(getConvFile());
             bufferedWriter = new BufferedWriter(fileWriter);
-            int read;
+  
             while ((read = readFile.read()) != -1) {
                 readedChar = Integer.toHexString(read);
                 readedCharLength = readedChar.length();
+                
                 if (readedCharLength == 1) {
                     readedChar = zeroChar.concat(readedChar);
                 }
+                
                 dataRow.add(readedChar);
+                
                 if (fileType == FILETYPE.BESTSERVER  && carriageReturnSensor == 3 && dataRow.size() == 10) {
                     for (String data : dataRow) {
                         bufferedWriter.write(data + " ");
                     }
+                    
                     bufferedWriter.newLine();
                     dataRow.clear();
                 }
+                
                 if (fileType == FILETYPE.NTHSERVER && carriageReturnSensor == 3) { 
                     if (dataRow.size() == numOfServersCharLength && isNumOfServersSet == false) {
                         numOfServers = new StringBuilder();
+                        
                         for (String data : dataRow) {
-                            char character = Tools.convertHex2Char(data);
+                            character = Tools.convertHex2Char(data);
+                            
                             if (character != zeroChar.charAt(0)) {
                                 numOfServers.append(character);
                             } else {
@@ -121,48 +134,59 @@ public class ConvertDatFile {
                                 }
                             }   
                         }
+                        
                         if (numOfServers.length() > 0) {
                             numOfServersInt = Integer.parseInt(numOfServers.toString());
                             isNumOfServersSet = true;
                         } else {
                             isNumOfServersSet = false;
+                            
                             for (int zeroRowCharCounter = 0; zeroRowCharCounter < 10; zeroRowCharCounter++) {
                                 bufferedWriter.write("00 ");
                             }
+                            
                             bufferedWriter.newLine();
                         }
+                        
                         dataRow.clear();   
                     } else {
                         if (dataRow.size() == 10 && serverCounter < numOfServersInt) {
                             for (String data : dataRow) {
                                 bufferedWriter.write(data + " ");
                             }
+                            
                             dataRow.clear();
                             serverCounter++;
+                            
                             if (serverCounter == numOfServersInt) {
                                 bufferedWriter.newLine();
                             }
                         }
+                        
                         if (serverCounter == numOfServersInt) {
                             serverCounter = 0;
                             isNumOfServersSet = false;
                         }
                     }  
                 }
+                
                 if (readedChar.equals(hexNewLine)) {
                     if (carriageReturnSensor < 3) {
                         if (dataRow.size() > emptyLineSize) {
                             headerRow = new StringBuilder();
                             dataRow.remove(hexNewLine);
                             dataRow.remove(hexCarriageReturn);
+                            
                             for (String data : dataRow) {
-                                char character = Tools.convertHex2Char(data);
+                                character = Tools.convertHex2Char(data);
                                 headerRow.append(character);
                                 bufferedWriter.write(character);
                             }
+                            
                             bufferedWriter.newLine();
                             String[] headerRowArray = headerRow.toString().trim().split(" ");
                             ArrayList<String> headerRowArrayList = new ArrayList<String>(Arrays.asList(headerRowArray));
+                            
                             if (headerRowArrayList.contains("BESTSERVER")) {
                                 fileType = FILETYPE.BESTSERVER;
                             } else if (headerRowArrayList.contains("NTHSERVER")) {
@@ -175,6 +199,7 @@ public class ConvertDatFile {
                             bufferedWriter.write("NO COMMENT");
                             bufferedWriter.newLine();
                         }
+                        
                         carriageReturnSensor++;
                         dataRow.clear();
                     }
@@ -198,27 +223,31 @@ public class ConvertDatFile {
     private void convertRawData() {
         FileWriter fileWriter = null;
         BufferedWriter bufferedWriter = null;
-        ArrayList<String> rawData = new ArrayList<String>();
+        ArrayList<String> rawData = null;
         ArrayList<String> arrToCsvFile = null;
+        List<String> dataPair = null;
         int dataRowNumFromRawData = 3;
         int hd72EovYcoordinate = 0;
         int hd72EovXcoordinate = 0;
-        int cellId = 0;
-        int signalStrength = 0;
-        String cellName = null;
         String headerRow = null;
         String rowToCsvFile = null;
         String[] mapDataRow = null;
-        String[] processedLine = null;
-        String[] hexCharBuffer = new String[10];
-        Hashtable<Object, Object> mapData = new Hashtable<>();
-        Hashtable<String,Integer> cellData = null;
+        String[] processedRow = null;
+        String[] hexCharBuffer = null;
+        Hashtable<Object, Object> mapData = null;
+        
+        mapData = new Hashtable<>();
+        rawData = new ArrayList<String>();
+        arrToCsvFile = new ArrayList<String>();
+        hexCharBuffer = new String[10];
         
         if (fileType == FILETYPE.BESTSERVER) {
             headerRow = "latitude,longitude,cellID,signalStrength";
         }
+        
         if (fileType == FILETYPE.NTHSERVER) {
             headerRow = "latitude,longitude,cellID,signalStrength";
+            
             for (int serverCounter = 1; serverCounter < maxNoOfServers; serverCounter++) {
                 String headerRowForNthServer = ",n" + serverCounter + "cellID,n" + serverCounter + "signalStrength";
                 headerRow = headerRow.concat(headerRowForNthServer);
@@ -238,50 +267,32 @@ public class ConvertDatFile {
             mapData.put("fileInfo", mapDataRow[5]);
             bufferedWriter.write(headerRow);
             bufferedWriter.newLine();
+            
             for (hd72EovYcoordinate = (int) mapData.get("northMax"); hd72EovYcoordinate > (int) mapData.get("northMin"); hd72EovYcoordinate = hd72EovYcoordinate - (int) mapData.get("resolution")) {
                 for (hd72EovXcoordinate = (int) mapData.get("eastMin"); hd72EovXcoordinate < (int) mapData.get("eastMax"); hd72EovXcoordinate = hd72EovXcoordinate + (int) mapData.get("resolution")) {
-                    arrToCsvFile = new ArrayList<String>();
+                    arrToCsvFile.clear();
                     rowToCsvFile = null;
                     arrToCsvFile.add(Integer.toString(hd72EovXcoordinate));
                     arrToCsvFile.add(Integer.toString(hd72EovYcoordinate));
                     
                     if (fileType == FILETYPE.BESTSERVER) {
-                        processedLine = rawData.get(dataRowNumFromRawData).split(" ");
-                        /*
-                         * ettol
-                         */
-                        cellData = getCellData(processedLine);
-                        cellId = cellData.get("cellID");
-                        cellName = convertCellID.get(Integer.toString(cellId));
-                        if (cellName != null) {
-                            arrToCsvFile.add(cellName);
-                        } else {
-                            arrToCsvFile.add(Integer.toString(cellId));
-                        }
-                        signalStrength = cellData.get("signalStrength");
-                        arrToCsvFile.add(Integer.toString(signalStrength));
-                        /*
-                         * eddig lehetne egy metodus
-                         */
+                        processedRow = rawData.get(dataRowNumFromRawData).split(" ");
+                        dataPair = getDataFromRow(processedRow);
+                        arrToCsvFile.addAll(dataPair);
                     }
+                    
                     if (fileType == FILETYPE.NTHSERVER) {
-                        processedLine = rawData.get(dataRowNumFromRawData).split(" ");
+                        processedRow = rawData.get(dataRowNumFromRawData).split(" ");
                         int hexCharCounter = 0;
-                        for (String hexChar : processedLine) {
+                        
+                        for (String hexChar : processedRow) {
                             if (hexCharCounter != 10) {
                                 hexCharBuffer[hexCharCounter] = hexChar;
                                 hexCharCounter++;
+                                
                                 if (hexChar.equals("ff") && hexCharCounter == 10) {
-                                    cellData = getCellData(hexCharBuffer);
-                                    cellId = cellData.get("cellID");
-                                    cellName = convertCellID.get(Integer.toString(cellId));
-                                    if (cellName != null) {
-                                        arrToCsvFile.add(cellName);
-                                    } else {                                        
-                                        arrToCsvFile.add(Integer.toString(cellId));
-                                    }
-                                    signalStrength = cellData.get("signalStrength");
-                                    arrToCsvFile.add(Integer.toString(signalStrength));
+                                    dataPair = getDataFromRow(hexCharBuffer);
+                                    arrToCsvFile.addAll(dataPair);
                                     hexCharCounter = 0;
                                 }
                             }
@@ -309,22 +320,49 @@ public class ConvertDatFile {
         }        
     }
     
+    private List<String> getDataFromRow(String[] row) {
+        Hashtable <String, Integer> cellData =  null;
+        List<String> dataPair = null;
+        int cellId = 0;
+        int signalStrength = 0;
+        String cellName = null;
+        
+        dataPair = new ArrayList<String>();
+        cellData = new Hashtable <String, Integer>();
+        
+        cellData = getCellData(row);
+        cellId = cellData.get("cellID");
+        cellName = convertCellID.get(Integer.toString(cellId));
+        
+        if (cellName != null) {
+            dataPair.add(cellName);   
+        } else {
+            dataPair.add(Integer.toString(cellId));
+        }
+        
+        signalStrength = cellData.get("signalStrength");
+        dataPair.add(Integer.toString(signalStrength));
+        
+        return dataPair;
+        
+    }
+    
     private Hashtable<String,Integer> getCellData(String[] processedCell){
-        String[] hexCellID = Arrays.copyOfRange(processedCell, 0, 4);
-        //String[] hexCellLayerID = Arrays.copyOfRange(processedCell, 4, 8);
-        String[] hexSignalStrength = Arrays.copyOfRange(processedCell, 8, 10);
-        int cellID = Tools.convertHex2Dec(Tools.reversArray(hexCellID));
-        //int cellLayerID = Tools.convertHex2Dec(Tools.reversArray(hexCellLayerID));
-        int signalStrength = Tools.convertHex2Dec(Tools.reversArray(hexSignalStrength));
-        @SuppressWarnings("serial")
-        Hashtable<String, Integer> cellData = new Hashtable<String, Integer>() {
-            {
-                put("cellID",cellID);
-                //put("cellLayerID",cellLayerID);
-                put("signalStrength",signalStrength);
-            }
-        };
-            
+        Hashtable<String, Integer> cellData = null;
+        int cellID = 0;
+        int signalStrength = 0;
+        String[] hexCellID = null;
+        String[] hexSignalStrength = null;
+        
+        cellData = new Hashtable<String, Integer>();
+        
+        hexCellID = Arrays.copyOfRange(processedCell, 0, 4);
+        hexSignalStrength = Arrays.copyOfRange(processedCell, 8, 10);
+        cellID = Tools.convertHex2Dec(Tools.reversArray(hexCellID));
+        signalStrength = Tools.convertHex2Dec(Tools.reversArray(hexSignalStrength));
+        cellData.put("cellID",cellID);
+        cellData.put("signalStrength",signalStrength);
+        
         return cellData;
     }
     
